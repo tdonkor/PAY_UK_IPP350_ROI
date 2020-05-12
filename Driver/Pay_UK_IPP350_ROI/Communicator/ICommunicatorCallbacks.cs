8cca8a9843ac327f6909fe5c2fbc2a7a55a2f1d4
tdonkor <tdonkor@gmail.com>
2020-05-12 13:57:20 +0100

    Add project files.

---
+++ Driver/Pay_UK_IPP350_ROI/Communicator/ICommunicatorCallbacks.cs
@@ -0,0 +1,11 @@
+﻿namespace PAY_UK_IPP350_ROI.Communicator
+{
+    public interface ICommunicatorCallbacks
+    {
+        void InitRequest(object parameters);
+
+        void TestRequest(object parameters);
+
+        void PayRequest(object parameters);
+    }
+}
---
+++ Driver/Pay_UK_IPP350_ROI/Program.cs
@@ -0,0 +1,28 @@
+﻿using Acrelec.Library.Logger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using PAY_UK_IPP350_ROI.Communicator;
+
+namespace PAY_UK_IPP350_ROI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CoreCommunicator coreCommunicator = new CoreCommunicator();
+
+            PayIngenicoIPP350_ROI payIngenicoIPP350_ROI = new PayIngenicoIPP350_ROI(coreCommunicator);
+
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            Log.Info(PayIngenicoIPP350_ROI.PAY_INGENICO_IPP350_ROI_LOG, "Pay Conexflow started.");
+        }
+
+    static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+    {
+        Log.Info(PayIngenicoIPP350_ROI.PAY_INGENICO_IPP350_ROI_LOG, "Unhandled Exception: " + (e.ExceptionObject as Exception).Message);
+    }
+}
+}
