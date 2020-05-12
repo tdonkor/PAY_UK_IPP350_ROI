7b1c8c6e2b2f9d6db3e7b8d2415b53125c5d7452
tdonkor <tdonkor@gmail.com>
2020-05-12 17:19:58 +0100

    Updated for ROI

---
+++ Driver/Pay_ROI_IPP350/Communicator/ICommunicatorCallbacks.cs
@@ -0,0 +1,11 @@
+﻿namespace PAY_ROI_IPP350.Communicator
+{
+    public interface ICommunicatorCallbacks
+    {
+        void InitRequest(object parameters);
+
+        void TestRequest(object parameters);
+
+        void PayRequest(object parameters);
+    }
+}
